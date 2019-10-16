58d32e4c1f78ddf01fd0ffbb42d7743ac7507829
lcparra <parra@ccny.cuny.edu>
2019-10-16 13:57:05 -0400

    Update nanRXY.m
    Had to make it match to the channels by samples structure of the rest of the code. Should ultimately be changed as it is less efficient the way it currently is.

--- nanRXY.m
+++ nanRXY.m
@@ -1,6 +1,6 @@
 function [Rxy,Rxx,Ryy,Ryx] = nanRXY(X,Y)
 % [Rxy,Rxx,Ryy,Ryx] = nanRXY(X,Y) computes covariance matrixes taking NaN into account. 
-% Assumes X and Y are organized as samples by channels. 
+X = X.'; Y = Y.'; % this just because the rest of the doce assumes it this way. 
 Rxx=nancov(X,'pairwise');
 Ryy=nancov(Y,'pairwise');
 for i=1:size(X,2), for j=1:size(Y,2), c=nancov(X(:,i),Y(:,i)); Rxy(i,j)=c(1,2); end; end;
